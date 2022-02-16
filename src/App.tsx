02084269a726aa8cd60dd28af703bee7a7b24ec3
khorh <helen.khor@googlemail.com>
2022-02-16 08:45:39 +0000

    added nav, divider and footer components as well as template for routes to each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Nav from "./components/organisms/Nav/Nav";
+import Footer from "./components/organisms/Footer/Footer";
+import Divider from "./components/atoms/Divider/Divider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <BrowserRouter>
+            <Nav />
+            <Routes>
+                <Route></Route>
+            </Routes>
+            <Divider />
+            <Footer />
+        </BrowserRouter>
+    );
+};
 
 export default App;
