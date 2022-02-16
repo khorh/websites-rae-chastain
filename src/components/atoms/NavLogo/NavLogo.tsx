db9efc24229ccf08724db686eb22dc6d61483f09
khorh <helen.khor@googlemail.com>
2022-02-16 08:26:25 +0000

    created nav logo component with responsive design

---
+++ src/components/atoms/NavLogo/NavLogo.tsx
@@ -0,0 +1,10 @@
+import "./NavLogo.scss";
+import Logo from "../../../assets/images/logo.png";
+
+const NavLogo = () => {
+    return (
+        <img className="nav__logo" src={Logo} alt="rae chastain's logo" />
+    );
+};
+
+export default NavLogo;
