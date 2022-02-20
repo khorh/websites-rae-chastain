e35d72664d6d9daef4e75c29bce4cf7246cdfd45
khorh <helen.khor@googlemail.com>
2022-02-20 18:48:58 +0000

    added contact route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Nav from "./components/organisms/Nav/Nav";
-import Footer from "./components/organisms/Footer/Footer";
+import Contact from "./components/pages/Contact/Contact";
 import Divider from "./components/atoms/Divider/Divider";
+import Footer from "./components/organisms/Footer/Footer";
+import Nav from "./components/organisms/Nav/Nav";
 
 function App() {
     return (
         <BrowserRouter>
             <Nav />
             <Routes>
-                <Route></Route>
+                <Route path="/contact" element={<Contact />} />
             </Routes>
             <Divider />
             <Footer />
