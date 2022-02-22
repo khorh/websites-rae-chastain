0c46c3bb327c8ed8ba05b79353c24d82138b700e
khorh <helen.khor@googlemail.com>
2022-02-22 12:01:09 +0000

    added about page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Nav from "./components/organisms/Nav/Nav";
+import About from "./components/pages/About/About";
 import Contact from "./components/pages/Contact/Contact";
 import Divider from "./components/atoms/Divider/Divider";
 import Footer from "./components/organisms/Footer/Footer";
-import Nav from "./components/organisms/Nav/Nav";
 
 function App() {
     return (
         <BrowserRouter>
             <Nav />
             <Routes>
+                <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
             <Divider />
