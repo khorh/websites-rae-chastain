58729a76f2b8e2906b3bcd7c6742e443c9f7d74c
khorh <helen.khor@googlemail.com>
2022-04-15 10:08:53 +0100

    moved logo out of atom folder

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,10 @@
+import "./Logo.scss";
+import Logo from "../../assets/images/logo.png";
+
+const LogoNav = () => {
+    return (
+        <img className="logo" src={Logo} alt="rae chastain's logo" />
+    );
+};
+
+export default LogoNav;
