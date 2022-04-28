62dd728f9e76664e28d5bcff025232f24d4b9e6d
khorh <helen.khor@googlemail.com>
2022-04-28 08:11:10 +0100

    updated import statements

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Nav from "./components/organisms/Nav/Nav";
 import About from "./components/pages/About/About";
 import Contact from "./components/pages/Contact/Contact";
-import Divider from "./components/Divider/Divider";
 import Footer from "./components/organisms/Footer/Footer";
+import Divider from './components/Divider/Divider';
 
 function App() {
     return (
