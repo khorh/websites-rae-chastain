0b1b147795aff61b50b98f78c83ca7a0f01b44f0
khorh <helen.khor@googlemail.com>
2022-04-28 07:59:37 +0100

    created story for logo

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Logo } from "./Logo";
+
+export default {
+    title: "Components/Logo",
+    component: Logo
+} as ComponentMeta<typeof Logo>;
+
+export const RaeChastain: ComponentStory<typeof Logo> = () => <Logo />
