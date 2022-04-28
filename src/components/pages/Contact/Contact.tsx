d5e2050b5c0334aa82ca6fd90e1cc5b01f9d8da8
khorh <helen.khor@googlemail.com>
2022-04-28 08:06:25 +0100

    added storyblok to get text information

--- src/components/pages/Contact/Contact.tsx
+++ src/components/pages/Contact/Contact.tsx
@@ -1,16 +1,42 @@
+import { useEffect, useState } from "react";
+import StoryblokClient from "storyblok-js-client";
 import "./Contact.scss";
 import PaperPlane from "../../../assets/images/paper-plane.jpg";
 
+let Storyblok = new StoryblokClient({
+    accessToken: "4UDPA39TEehWMG9eE631oAtt"
+});
+
 const Contact = () => {
+    const [contactData, setContactData] = useState<string>();
+
+    // useEffect(() => {
+    //     Storyblok.get("cdn/stories/127058094", {})
+    //         .then(response => {
+    //             setContactData(response.data.story.content.contact_info);
+    //         }).catch(error => {
+    //             console.log(error)
+    //         })
+    // }, []);
+
+
+
+    useEffect(() => {
+        Storyblok.get("cdn/stories/127058094", {})
+            .then(response => {
+                setContactData(response.data.story.content.contact_info);
+            }).catch(error => {
+                console.log(error)
+            })
+    }, []);
+
     return (
         <main className="contact">
-            <h1 className="contact__title">Contact</h1>
             <section>
                 <img className="contact__image" src={PaperPlane} alt="people throwing paper plane" />
             </section>
             <section className="contact__text">
-                <p>If you have media queries about my books or want to discuss illustrations for your own books or merchandise,
-                    please contact me using books@readchastain.com</p>
+                <p>{contactData}</p>
             </section>
         </main>
     );
