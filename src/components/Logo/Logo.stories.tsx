83a1ab8f6084ca41ad95185ba55fe1515ade644c
khorh <helen.khor@googlemail.com>
2022-05-01 08:59:55 +0100

    added description

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -3,7 +3,14 @@ import Logo from "./Logo";
 
 export default {
     title: "Components/Logo",
-    component: Logo
+    component: Logo,
+    parameters: {
+        docs: {
+          description: {
+            component: "A logo represents the business.",
+          },
+        },
+      },
 } as ComponentMeta<typeof Logo>;
 
 export const RaeChastain: ComponentStory<typeof Logo> = () => <Logo />
