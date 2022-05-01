df710fa5e4ee4744ac955765c0fe6d1d94368a31
khorh <helen.khor@googlemail.com>
2022-05-01 08:12:43 +0100

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Nav from "./components/organisms/Nav/Nav";
 import About from "./components/pages/About/About";
