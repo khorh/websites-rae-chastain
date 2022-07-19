70e0a9c79720ac117c3306684938966f13cdfa11
khorh <helen.khor@googlemail.com>
2022-07-19 09:14:32 +0100

    updated to ES6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Nav from "./components/Nav/Nav";
-import About from "./components/pages/About/About";
-import Contact from "./components/pages/Contact/Contact";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Divider from './components/Divider/Divider';
 
-function App() {
+const App = () => {
     return (
         <BrowserRouter>
             <Nav />
