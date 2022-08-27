6e0e887584d3456c27fa3ebb0f5ed3a0787ca1d9
khorh <helen.khor@googlemail.com>
2022-08-27 10:30:44 +0100

    deleted description

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -4,13 +4,6 @@ import Logo from "./Logo";
 export default {
     title: "Components/Logo",
     component: Logo,
-    parameters: {
-        docs: {
-          description: {
-            component: "A logo represents the business.",
-          },
-        },
-      },
 } as ComponentMeta<typeof Logo>;
 
 export const RaeChastain: ComponentStory<typeof Logo> = () => <Logo />
