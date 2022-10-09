34042a388965db47b2ea570977ed58f4bcf56411
khorh <helen.khor@googlemail.com>
2022-10-09 08:53:37 +0100

    updated footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,10 @@
-import "./Footer.scss";
+import styles from "./Footer.module.scss";
 import Icon from "../Icon/Icon";
 import Paragraph from "../Text/Paragraph/Paragraph";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className={`${styles.base}`}>
       <Paragraph size="medium" weight={500}>
         © Rae Chastain
       </Paragraph>
