1cdd496813818c250ee71579cdd23bdf80921550
khorh <helen.khor@googlemail.com>
2022-10-09 09:00:34 +0100

    moved divider from App.tsx to Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import Divider from './components/Divider/Divider';
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Nav />
-            <Routes>
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-            <Divider />
-            <Footer />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import styles from "./Footer.module.scss";
+import Divider from "../Divider/Divider";
 import Icon from "../Icon/Icon";
 import Paragraph from "../Text/Paragraph/Paragraph";
 
 const Footer = () => {
   return (
     <footer className={`${styles.base}`}>
+      <Divider />
       <Paragraph size="medium" weight={500}>
         © Rae Chastain
       </Paragraph>
