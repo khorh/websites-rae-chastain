fdcfe665f24b8c161183914fbb162fc31b01b369
khorh <helen.khor@googlemail.com>
2022-10-10 08:36:11 +0100

    added spacing as a prop

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,7 +1,18 @@
+import { FC } from "react";
 import styles from "./Divider.module.scss";
+import "../../styles/main.scss";
 
-const Divider = () => {
-  return <hr className={`${styles.base}`} />;
+export interface DividerProps {
+  marginBottom?: 4 | 8 | 16 | 24 | 32 | 48;
+}
+
+const Divider: FC<DividerProps> = (props) => {
+  // Defaults
+  const marginBottom = props.marginBottom
+    ? `marginBottom__${props.marginBottom}`
+    : "marginBottom__0";
+
+  return <hr className={`${styles.base} ${styles[marginBottom]}`} />;
 };
 
 export default Divider;
