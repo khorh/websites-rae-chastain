d8f273d8d239c4c4ab4da284eb8ce5e88c47d083
khorh <helen.khor@googlemail.com>
2022-10-11 08:07:21 +0100

    created separate file for icons list

---
+++ src/components/Icon/IconList.ts
@@ -0,0 +1,19 @@
+import { ReactComponent as Facebook } from "../../assets/icons/facebook.svg";
+import { ReactComponent as Instagram } from "../../assets/icons/instagram.svg";
+
+const iconList = [
+  {
+    name: "facebook",
+    href: "https://www.facebook.com/readchastain",
+    src: Facebook,
+    alt: "facebook logo",
+  },
+  {
+    name: "instagram",
+    href: "https://www.instagram.com/readchastain",
+    src: Instagram,
+    alt: "instagram logo",
+  },
+];
+
+export default iconList;
