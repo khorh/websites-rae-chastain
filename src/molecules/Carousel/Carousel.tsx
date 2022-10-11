49462f63292d806059b7c09fd0e3c9113396cb6f
khorh <helen.khor@googlemail.com>
2022-10-11 16:54:38 +0100

    updated carousel component

--- src/molecules/Carousel/Carousel.tsx
+++ src/molecules/Carousel/Carousel.tsx
@@ -1,14 +1,22 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./Carousel.module.scss";
 import Icon from "../../components/Icon/Icon";
 
 const Carousel: FC = () => {
+  const [toggleIcon, setToggleIcon] = useState<boolean>(true);
+
+  const togglePlayPause = () => {
+    setToggleIcon(!toggleIcon);
+  };
+
   return (
-    <div>
-      <Icon name="play" />
-      <div className={styles.container__dot}>
-        <Icon name="dot" />
-        <Icon name="dot" />
+    <div className={styles.container}>
+      <div className={styles.container__playPause} onClick={togglePlayPause}>
+        {toggleIcon ? <Icon name="play" /> : <Icon name="pause" />}
+      </div>
+      <div className={styles.container__dots}>
+        <Icon name="dot" marginRight={8} />
+        <Icon name="dot" marginRight={8} />
         <Icon name="dot" />
       </div>
     </div>
