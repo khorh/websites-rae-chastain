da8a77e2628f933ca96ec901882b416c78ecd948
khorh <helen.khor@googlemail.com>
2022-10-11 12:47:58 +0100

    updated with margin bottom prop

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -2,8 +2,8 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Logo from "./Logo";
 
 export default {
-    title: "Components/Logo",
-    component: Logo,
+  title: "Components/Logo",
+  component: Logo,
 } as ComponentMeta<typeof Logo>;
 
-export const RaeChastain: ComponentStory<typeof Logo> = () => <Logo />
+export const RaeChastain: ComponentStory<typeof Logo> = () => <Logo />;
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,23 @@
+import { FC } from "react";
 import styles from "./Logo.module.scss";
 import LogoRaeChastain from "../../assets/images/logo.png";
 
-const Logo = () => {
+export interface LogoProps {
+  /**
+   * What is the spacing at the bottom?
+   */
+  marginBottom?: 4 | 8 | 16 | 24 | 32 | 48;
+}
+
+const Logo: FC<LogoProps> = (props) => {
+  // Defaults
+  const marginBottom = props.marginBottom
+    ? `marginBottom__${props.marginBottom}`
+    : "marginBottom__0";
+
   return (
     <img
-      className={`${styles.base}`}
+      className={`${styles.base} ${marginBottom}`}
       src={LogoRaeChastain}
       alt="rae chastain's logo"
     />
