f8c6b49aa5dc4d13a22980805436131ce8bd227d
khorh <helen.khor@googlemail.com>
2022-10-11 14:06:22 +0100

    updated navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,43 +1,43 @@
 import { FC } from "react";
 import "./Nav.scss";
+import Logo from "../Logo/Logo";
 import NavDropdownMenu from "./NavDropdownMenu/NavDropdownMenu";
 import NavHorizontalMenu from "./NavHorizontalMenu/NavHorizontalMenu";
-import Logo from "../Logo/Logo";
 import { navMenuContentProps } from "../../interfaces/interfaces";
 
 const Nav: FC<navMenuContentProps> = () => {
-    const navMenuContent = [
-        {
-            label: "Books",
-            link: "/home"
-        },
-        {
-            label: "Illustrations",
-            link: "/illustrations"
-        },
-        {
-            label: "Blog",
-            link: "/blog"
-        },
-        {
-            label: "About",
-            link: "/about"
-        },
-        {
-            label: "Contact",
-            link: "/contact"
-        }
-    ];
+  const navMenuContent = [
+    {
+      label: "Books",
+      link: "/home",
+    },
+    {
+      label: "Illustrations",
+      link: "/illustrations",
+    },
+    {
+      label: "Blog",
+      link: "/blog",
+    },
+    {
+      label: "About",
+      link: "/about",
+    },
+    {
+      label: "Contact",
+      link: "/contact",
+    },
+  ];
 
-    return (
-        <nav className="nav">
-            <Logo />
-            <div className="nav__dropdown">
-                <NavDropdownMenu navMenuContent={navMenuContent} />
-            </div>
-            <NavHorizontalMenu navMenuContent={navMenuContent} />
-        </nav>
-    );
+  return (
+    <nav className="nav">
+      <Logo marginBottom={16} />
+      <div className="nav__dropdown">
+        <NavDropdownMenu navMenuContent={navMenuContent} />
+      </div>
+      <NavHorizontalMenu navMenuContent={navMenuContent} />
+    </nav>
+  );
 };
 
 export default Nav;
