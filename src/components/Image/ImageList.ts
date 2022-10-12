9d95e3b72d0725b56ea6201781b2f4a5d1957e84
khorh <helen.khor@googlemail.com>
2022-10-12 08:03:20 +0100

    created separate file for list of images

---
+++ src/components/Image/ImageList.ts
@@ -0,0 +1,25 @@
+import WomanDesigning from "../../assets/images/woman-designing.jpg";
+import PaperPlane from "../../assets/images/paper-plane.jpg";
+import TheClawby from "../../assets/images/the-clawby.jpg";
+import TheOctopusAndTheMermaid from "../../assets/images/the-octopus-and-the-mermaid.jpg";
+
+const ImageList = [
+  {
+    name: "womanDesigning",
+    src: WomanDesigning,
+    alt: "woman designing",
+  },
+  {
+    name: "paperPlane",
+    src: PaperPlane,
+    alt: "people throwing paper plane",
+  },
+  { name: "the-clawby", src: TheClawby, alt: "the clawby book cover" },
+  {
+    name: "the-octopus-and-the-mermaid",
+    src: TheOctopusAndTheMermaid,
+    alt: "the octopus and the mermaid book cover",
+  },
+];
+
+export default ImageList;
