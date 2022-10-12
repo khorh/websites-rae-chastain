8f352dc33401cbf4beabe791cd32cbbc47b868a8
khorh <helen.khor@googlemail.com>
2022-10-12 07:36:42 +0100

    updated with books route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./organisms/Footer/Footer";
 import Nav from "./organisms/Nav/Nav";
 import About from "./pages/About/About";
+import Books from "./pages/Books/Books";
 import Contact from "./pages/Contact/Contact";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <BrowserRouter>
       <Nav />
       <Routes>
+        <Route path="/books" element={<Books />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
