1d8aff3b6edf7c98dd1b772667f81be51dd92b1e
khorh <helen.khor@googlemail.com>
2022-10-13 08:27:29 +0100

    updated image component for books hero image

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,46 +1,27 @@
-import "./Image.scss";
-import WomanDesigning from "../../assets/images/woman-designing.jpg";
-import PaperPlane from "../../assets/images/paper-plane.jpg";
+import { FC } from "react";
+import styles from "./Image.module.scss";
+import ImageList from "./ImageList";
 
 interface ImageProps {
-    /**
-    * What is the image's name?
-    */
-    name?: string,
+  /**
+   * What is the image's name?
+   */
+  name: string;
 }
 
-const Image = ({
-    name,
-    ...props
-} : ImageProps) => {
-    const ImageList = [
-        {
-            name: "womanDesigning",
-            src: WomanDesigning,
-            alt: "woman designing"
-        },
-        {
-            name: "paperPlane",
-            src: PaperPlane,
-            alt: "people throwing paper plane"
-        }
-    ];
+const Image: FC<ImageProps> = (props) => {
+  // Method to filter and display the image
+  const FilteredImage = ImageList.filter(
+    (filter) => filter.name === props.name
+  );
 
-    const ImageFilteredName = ImageList.filter(selection => selection.name === name);
-
-    const ImageDisplay = ImageFilteredName.map(image => {
-        return (
-            <section key={image.name} className="image__container">
-                <img className="image__hero" src={image.src} alt={image.alt} />
-            </section>
-        );
-    });
-
+  const DisplayImage = FilteredImage.map((display) => {
     return (
-        <>
-            {ImageDisplay}
-        </>
+      <img className={styles.books__hero} src={display.src} alt={display.alt} />
     );
+  });
+
+  return <>{DisplayImage}</>;
 };
 
 export default Image;
