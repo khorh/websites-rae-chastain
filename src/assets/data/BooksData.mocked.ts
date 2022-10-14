7ffb76c3487a8c6665db56fe76e84738da65f041
khorh <helen.khor@googlemail.com>
2022-10-14 09:33:32 +0100

    created mocked data for books

---
+++ src/assets/data/BooksData.mocked.ts
@@ -0,0 +1,13 @@
+const BooksData = [
+  {
+    bookName: "The Octopus and the Mermaid",
+    shortDescription:
+      "Join this enchanting story of how a lonely coconut octopus is befriended by a beautiful mermaid.",
+    imageName: "the-octopus-and-the-mermaid-cover",
+    heroBook: true,
+    orderLink: "",
+    detailsLink: "",
+  },
+];
+
+export default BooksData;
