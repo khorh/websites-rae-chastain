fc62f762bef66dc00ef3b248a071a6c57617d2ed
khorh <helen.khor@googlemail.com>
2022-10-16 08:11:54 +0100

    starting to create details page of a book

---
+++ src/organisms/BookDetailsHeader/BookDetailsHeader.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import Button from "../../components/Button/Button";
+import Heading from "../../components/Text/Heading/Heading";
+
+const BookDetailsHeader: FC = () => {
+  return (
+    <header>
+      <Heading type="h1" font="primary" marginBottom={24}>
+        The Clawby
+      </Heading>
+      <Button type="primary">Order</Button>
+    </header>
+  );
+};
+
+export default BookDetailsHeader;
