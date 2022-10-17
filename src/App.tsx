23ad42a8f05ea1311a4d8f9e5499702355c46995
khorh <helen.khor@googlemail.com>
2022-10-17 12:16:11 +0100

    added blog as a route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./organisms/Footer/Footer";
 import Nav from "./organisms/Nav/Nav";
 import About from "./pages/About/About";
+import Blog from "./pages/Blog/Blog";
 import Books from "./pages/Books/Books";
 import Contact from "./pages/Contact/Contact";
 import Illustrations from "./pages/Illustrations/Illustrations";
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Books />} />
         <Route path="/illustrations" element={<Illustrations />} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
