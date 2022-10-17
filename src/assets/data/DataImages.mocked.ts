16a98605a66d3fdfdec833cbac731c620c247826
khorh <helen.khor@googlemail.com>
2022-10-17 09:37:25 +0100

    added new images into mocked data

--- src/assets/data/DataImages.mocked.ts
+++ src/assets/data/DataImages.mocked.ts
@@ -2,10 +2,14 @@ import WomanDesigning from "../../assets/images/woman-designing.jpg";
 import PaperPlane from "../../assets/images/paper-plane.jpg";
 import BoysWillBeBoysCover from "../../assets/images/boys-will-be-boys-cover.jpg";
 import BreezyNoProbLlamaCover from "../../assets/images/breezy-no-prob-llama-cover.jpg";
+import FrancisBumblebee from "../../assets/images/francis-bumblebee.jpg";
+import GrandmotherGoose from "../../assets/images/grandmother-goose.jpg";
+import MermaidAndDolphin from "../../assets/images/mermaid-and-dolphin.jpg";
+import Mermaid from "../../assets/images/mermaid.jpg";
+import SirMousealotCover from "../../assets/images/sir-mousealot-cover.jpg";
 import TheClawbyCover from "../../assets/images/the-clawby-cover.jpg";
 import TheOctopusAndTheMermaidCover from "../../assets/images/the-octopus-and-the-mermaid-cover.jpg";
 import TheTurtlesNewShellCover from "../../assets/images/the-turtles-new-shell-cover.jpg";
-import SirMousealotCover from "../../assets/images/sir-mousealot-cover.jpg";
 
 const DataImages = [
   {
@@ -58,11 +62,39 @@ const DataImages = [
     alt: "the turtle's new shell placeholder cover",
   },
   {
-    id: "image007",
+    id: "image008",
     category: "books",
     name: "sir-mousealot-cover",
     src: SirMousealotCover,
     alt: "sir mousealot placeholder cover",
+  },
+  {
+    id: "image009",
+    category: "illustrations",
+    name: "francis-bumblebee",
+    src: FrancisBumblebee,
+    alt: "francis bumblebee illustration",
+  },
+  {
+    id: "image010",
+    category: "illustrations",
+    name: "grandmother-goose",
+    src: GrandmotherGoose,
+    alt: "grandmother goose illustration",
+  },
+  {
+    id: "image011",
+    category: "illustrations",
+    name: "mermaid-and-dolphin",
+    src: MermaidAndDolphin,
+    alt: "mermaid and dolphin illustration",
+  },
+  {
+    id: "image012",
+    category: "illustrations",
+    name: "mermaid",
+    src: Mermaid,
+    alt: "mermaid illustration",
   },
 ];
 
