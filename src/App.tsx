98b8dc4a98f7c81f0def85c596c066bb6010990b
khorh <helen.khor@googlemail.com>
2022-10-17 09:13:20 +0100

    added route for illustrations

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import Nav from "./organisms/Nav/Nav";
 import About from "./pages/About/About";
 import Books from "./pages/Books/Books";
 import Contact from "./pages/Contact/Contact";
+import Illustrations from "./pages/Illustrations/Illustrations";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/about" element={<About />} />
         <Route path="/" element={<Books />} />
+        <Route path="/illustrations" element={<Illustrations />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
