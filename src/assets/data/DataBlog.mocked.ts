fd85b0b2dc3751368ae7dd6fd0ba923e9f5190d8
khorh <helen.khor@googlemail.com>
2022-10-17 13:50:32 +0100

    created data for blog

---
+++ src/assets/data/DataBlog.mocked.ts
@@ -0,0 +1,49 @@
+const DataBlog = [
+  {
+    id: "blog001",
+    name: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis ex sit amet sapien interdum molestie. Etiam ut lorem quis odio tincidunt molestie sed eu nibh. Etiam nec aliquam arcu, commodo facilisis mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hendrerit metus et ipsum mollis eleifend. Aliquam sed quam consectetur, euismod massa vitae, varius ex. Quisque lacinia magna vel sem maximus, finibus posuere lectus imperdiet. Mauris molestie mauris sit amet ante tempor, in consequat nulla convallis. Sed interdum ex in nisi elementum, et pretium lacus auctor.",
+    imageName: "grandmother-goose",
+    datePosted: "1 Nov 22",
+    link: "",
+  },
+  {
+    id: "blog002",
+    name: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis ex sit amet sapien interdum molestie. Etiam ut lorem quis odio tincidunt molestie sed eu nibh. Etiam nec aliquam arcu, commodo facilisis mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hendrerit metus et ipsum mollis eleifend. Aliquam sed quam consectetur, euismod massa vitae, varius ex. Quisque lacinia magna vel sem maximus, finibus posuere lectus imperdiet. Mauris molestie mauris sit amet ante tempor, in consequat nulla convallis. Sed interdum ex in nisi elementum, et pretium lacus auctor.",
+    imageName: "grandmother-goose",
+    datePosted: "1 Dec 22",
+    link: "",
+  },
+  {
+    id: "blog003",
+    name: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis ex sit amet sapien interdum molestie. Etiam ut lorem quis odio tincidunt molestie sed eu nibh. Etiam nec aliquam arcu, commodo facilisis mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hendrerit metus et ipsum mollis eleifend. Aliquam sed quam consectetur, euismod massa vitae, varius ex. Quisque lacinia magna vel sem maximus, finibus posuere lectus imperdiet. Mauris molestie mauris sit amet ante tempor, in consequat nulla convallis. Sed interdum ex in nisi elementum, et pretium lacus auctor.",
+    imageName: "grandmother-goose",
+    datePosted: "1 Jan 23",
+    link: "",
+  },
+  {
+    id: "blog004",
+    name: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis ex sit amet sapien interdum molestie. Etiam ut lorem quis odio tincidunt molestie sed eu nibh. Etiam nec aliquam arcu, commodo facilisis mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hendrerit metus et ipsum mollis eleifend. Aliquam sed quam consectetur, euismod massa vitae, varius ex. Quisque lacinia magna vel sem maximus, finibus posuere lectus imperdiet. Mauris molestie mauris sit amet ante tempor, in consequat nulla convallis. Sed interdum ex in nisi elementum, et pretium lacus auctor.",
+    imageName: "grandmother-goose",
+    datePosted: "1 Feb 23",
+    link: "",
+  },
+  {
+    id: "blog005",
+    name: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis ex sit amet sapien interdum molestie. Etiam ut lorem quis odio tincidunt molestie sed eu nibh. Etiam nec aliquam arcu, commodo facilisis mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hendrerit metus et ipsum mollis eleifend. Aliquam sed quam consectetur, euismod massa vitae, varius ex. Quisque lacinia magna vel sem maximus, finibus posuere lectus imperdiet. Mauris molestie mauris sit amet ante tempor, in consequat nulla convallis. Sed interdum ex in nisi elementum, et pretium lacus auctor.",
+    imageName: "grandmother-goose",
+    datePosted: "1 Mar 23",
+    link: "",
+  },
+];
+
+export default DataBlog;
